4ff817562e691155b754c14384f64170a4107772
salmankhalid187 <salmankhalid1@gmail.com>
2020-03-11 12:09:23 +0500

    Missing files added

---
+++ lib/addProductsExample/product_control.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class  ProductControl extends StatelessWidget {
+  
+  final Function addProduct;
+
+  ProductControl(this.addProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+            color: Theme.of(context).primaryColor,
+            onPressed: () {
+              this.addProduct('Flash drive');
+            },
+            child: Text("Add Product"),
+          );
+  }
+
+}
